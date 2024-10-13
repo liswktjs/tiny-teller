18a47fde9acbc2d8f9807e225282e7e10b8f72d5
JASUN LEE <liswktjs@gmail.com>
2024-10-13 22:37:14 +0900

    feat: Card 컴포넌트 구현

--- packages/ui/src/Card/index.tsx
+++ packages/ui/src/Card/index.tsx
@@ -1,9 +1,25 @@
-const Card = () => {
-  return (
-    <div className="bg-slate-500">
-      Card
-    </div>
-  );
-};
+import { forwardRef, PropsWithChildren } from "react";
+import { HTMLMotionProps, motion } from "framer-motion";
+
+interface Props extends HTMLMotionProps<"div"> {
+  id: string;
+}
+
+const Card = forwardRef<HTMLDivElement, PropsWithChildren<Props>>(
+  ({ children, className, style, ...rest }, ref) => {
+    return (
+      <motion.div
+        className={`${className} rounded-lg shadow-2xl w-40 h-72 bg-white flex items-center justify-center`}
+        ref={ref}
+        style={{
+          ...style,
+        }}
+        {...rest}
+      >
+        {children}
+      </motion.div>
+    );
+  },
+);
 
 export default Card;
